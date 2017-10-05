8c4d364d28fb4b85ee5d79a216367e8c24e402d7
aits <aits@aitslu178>
2017-10-05 19:02:38 +0530

    table created

---
+++ src/main/java/com/aits/app/model/Employee.java
@@ -0,0 +1,49 @@
+package com.aits.app.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="employee")
+public class Employee implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "empID")
+	private int empID;
+
+	
+	@Column(name = "empName")
+	private String empName;
+	
+	public int getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	
+}
